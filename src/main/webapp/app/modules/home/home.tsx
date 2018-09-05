4f0e63c99e5536c8cb2884852f41bf564653025a
Nicholas Jarzembowski <nicholas.jarzembowski@aexp.com>
2018-09-05 16:03:51 +0100

    added style to user home page.

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Redirect, RouteComponentProps } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Row, Col, Alert, Input, Table, Form, FormGroup, FormText, Label, Button, CardColumns, Progress, Card, CardImg, CardBody, CardDeck, CardTitle, CardLink, CardSubtitle, CardText } from 'reactstrap';
+import { Row, Col, Alert, Input, Table, Form, FormGroup, FormText, Label, Badge, Button, CardColumns, Progress, Card, CardImg, CardBody, CardDeck, CardTitle, CardLink, CardSubtitle, CardText } from 'reactstrap';
 
 import { IRootState } from 'app/shared/reducers';
 import { getSession } from 'app/shared/reducers/authentication';
@@ -24,6 +24,29 @@ export class Home extends React.Component<IHomeProp> {
       <span className="dot"></span>
     </div>
   }
+  languages = () => (
+    <Progress className="card-languages" multi>
+      <Progress bar value="50">Java</Progress>
+      <Progress bar color="success" value="30">Javascript</Progress>
+      <Progress bar color="info" value="20">Typescript</Progress>
+    </Progress>
+  );
+  technologies = () => (
+    <div>
+      <Badge color="info" pill>Spring</Badge>
+      <Badge color="info" pill>React</Badge>
+      <Badge color="info" pill>Dashboard</Badge>
+    </div>
+  );
+  cardParticipants = () => (
+    <div className="card-participants">
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"><p>+100</p></span>
+    </div>
+  )
 
   render() {
     const { isAuthenticated } = this.props;
@@ -33,32 +56,39 @@ export class Home extends React.Component<IHomeProp> {
     return (
       <Row className="projects-container">
         <div>
+
+          <hr />
+          <h4>Our Ecosystem</h4>
+          <Progress className="card-languages" multi>
+            <Progress bar value="50">Java</Progress>
+            <Progress bar color="success" value="30">Javascript</Progress>
+            <Progress bar color="info" value="20">Typescript</Progress>
+          </Progress>
+
+          <Progress className="card-languages" multi>
+            <Progress bar value="25">Spring</Progress>
+            <Progress bar color="success" value="60">React</Progress>
+            <Progress bar color="info" value="15g">Play</Progress>
+          </Progress>
+
           <hr />
 
           <h4>Current Projects</h4>
           <h5>10 Results</h5>
 
-          <CardColumns>
+          <CardColumns className="projects-current-container">
             <Card>
-              <Progress multi>
-                <Progress bar value="50">Java</Progress>
-                <Progress bar color="success" value="30">Javascript</Progress>
-                <Progress bar color="info" value="20">Typescript</Progress>
-              </Progress>
-              <div className="card-participants">
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
-              </div>
-              {/*<CardImg className="card-image" top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />*/}
               <CardBody>
                 <CardTitle>Card title</CardTitle>
                 <CardSubtitle>Card subtitle</CardSubtitle>
+                {this.languages()}
+                {this.technologies()}
+                {this.cardParticipants()}
                 <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                <Button>Button</Button>
+                <Button className="project-explore-button">Explore</Button>
               </CardBody>
             </Card>
+
 
             {/*<Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>*/}
               {/*<CardTitle>Special Title Treatment</CardTitle>*/}
@@ -66,33 +96,30 @@ export class Home extends React.Component<IHomeProp> {
               {/*<Button>Button</Button>*/}
             {/*</Card>*/}
           </CardColumns>
+
           <hr />
 
           <h4>Recommended Projects</h4>
-          <Table>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Project Name</th>
-                <th>Project Owner</th>
-                <th>Technologies</th>
-                <th>Status</th>
-                <th>Issues</th>
-                <th>Committers</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-              </tr>
-            </tbody>
-          </Table>
+          <CardColumns className="projects-current-container">
+            <Card>
+              <CardBody>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                {this.languages()}
+                {this.technologies()}
+                {this.cardParticipants()}
+                <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                <Button className="project-explore-button">Explore</Button>
+              </CardBody>
+            </Card>
+
+            {/*<Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>*/}
+            {/*<CardTitle>Special Title Treatment</CardTitle>*/}
+            {/*<CardText>With supporting text below as a natural lead-in to additional content.</CardText>*/}
+            {/*<Button>Button</Button>*/}
+            {/*</Card>*/}
+          </CardColumns>
+
         </div>
       </Row>
     );
