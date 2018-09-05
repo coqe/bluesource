f120746f5e9d8e21a2f8d4432c05257ad241b6a6
Richard Selby <richard.a.selby@aexp.com>
2018-09-05 12:42:11 +0100

    added fields to create project page.

--- src/main/webapp/app/modules/create-project/create-project.tsx
+++ src/main/webapp/app/modules/create-project/create-project.tsx
@@ -6,14 +6,41 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Row, Col, Alert, Input, Table, Form, FormGroup, FormText, Label, Button } from 'reactstrap';
 
+import TagsInput from 'react-tagsinput'
+import 'react-tagsinput/react-tagsinput.css' // If using WebPack and style-loader.
+
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
 import { IRootState } from 'app/shared/reducers';
 import { getSession } from 'app/shared/reducers/authentication';
 
 export interface ICreateProjectProp extends StateProps, DispatchProps {}
 
 export class CreateProject extends React.Component<ICreateProjectProp> {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      tags: [],
+      startDate: moment()
+    }
+  }
+
   componentDidMount() {
     this.props.getSession();
+  }
+
+  handleChange(tags) {
+    this.setState({tags})
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
   }
 
   render() {
@@ -31,63 +58,33 @@ export class CreateProject extends React.Component<ICreateProjectProp> {
               <Label for="projectName">Project Name</Label>
               <Input type="projectName" name="projectName" id="projectName" placeholder="your project name" />
             </FormGroup>
+
             <FormGroup>
               <Label for="projectDescription">Project Description</Label>
               <Input type="textarea" name="text" id="exampleText" />
             </FormGroup>
             <FormGroup>
+              <Label for="dateNeeded">Date Needed (Optional)</Label>
+              <DatePicker
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
               <Label for="technologies">Technologies</Label>
-              <Input type="input" />
+              <TagsInput value={this.state.tags} onChange={this.handleChange} />
             </FormGroup>
+
             <FormGroup>
-              <Label for="exampleSelectMulti">Select Multiple</Label>
-              <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </Input>
+              <Label for="gitRepo">Git Repo</Label>
+              <Input type="gitRepo" name="gitRepo" id="gitRepo" placeholder="your git repo url" />
             </FormGroup>
+
             <FormGroup>
-              <Label for="exampleText">Text Area</Label>
-              <Input type="textarea" name="text" id="exampleText" />
+              <Label for="gitRepo">Add User</Label>
+              <Input type="gitRepo" name="gitRepo" id="gitRepo" placeholder="add users you want added to the project" />
             </FormGroup>
-            <FormGroup>
-              <Label for="exampleFile">File</Label>
-              <Input type="file" name="file" id="exampleFile" />
-              <FormText color="muted">
-                This is some placeholder block-level help text for the above input.
-                It's a bit lighter and easily wraps to a new line.
-              </FormText>
-            </FormGroup>
-            <FormGroup tag="fieldset">
-              <legend>Radio Buttons</legend>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="radio1" />{' '}
-                  Option one is this and that—be sure to include why it's great
-                </Label>
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="radio1" />{' '}
-                  Option two can be something else and selecting it will deselect option one
-                </Label>
-              </FormGroup>
-              <FormGroup check disabled>
-                <Label check>
-                  <Input type="radio" name="radio1" disabled />{' '}
-                  Option three is disabled
-                </Label>
-              </FormGroup>
-            </FormGroup>
-            <FormGroup check>
-              <Label check>
-                <Input type="checkbox" />{' '}
-                Check me out
-              </Label>
-            </FormGroup>
+
             <Button>Submit</Button>
           </Form>
 
