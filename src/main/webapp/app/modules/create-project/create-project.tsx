f318c21e038d69e31cc7327bfddfd5cc1bbdafe7
Richard Selby <richard.a.selby@aexp.com>
2018-09-05 11:37:32 +0100

    Added Create Project Page.

---
+++ src/main/webapp/app/modules/create-project/create-project.tsx
@@ -0,0 +1,113 @@
+import './create-project.css';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { Row, Col, Alert, Input, Table, Form, FormGroup, FormText, Label, Button } from 'reactstrap';
+
+import { IRootState } from 'app/shared/reducers';
+import { getSession } from 'app/shared/reducers/authentication';
+
+export interface ICreateProjectProp extends StateProps, DispatchProps {}
+
+export class CreateProject extends React.Component<ICreateProjectProp> {
+  componentDidMount() {
+    this.props.getSession();
+  }
+
+  render() {
+    const { account } = this.props;
+    return (
+      <Row>
+        <div>
+
+          <h4>Create a Project</h4>
+
+          <hr/>
+
+          <Form>
+            <FormGroup>
+              <Label for="projectName">Project Name</Label>
+              <Input type="projectName" name="projectName" id="projectName" placeholder="your project name" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="projectDescription">Project Description</Label>
+              <Input type="textarea" name="text" id="exampleText" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="technologies">Technologies</Label>
+              <Input type="input" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleSelectMulti">Select Multiple</Label>
+              <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleText">Text Area</Label>
+              <Input type="textarea" name="text" id="exampleText" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="exampleFile">File</Label>
+              <Input type="file" name="file" id="exampleFile" />
+              <FormText color="muted">
+                This is some placeholder block-level help text for the above input.
+                It's a bit lighter and easily wraps to a new line.
+              </FormText>
+            </FormGroup>
+            <FormGroup tag="fieldset">
+              <legend>Radio Buttons</legend>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio1" />{' '}
+                  Option one is this and that—be sure to include why it's great
+                </Label>
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio1" />{' '}
+                  Option two can be something else and selecting it will deselect option one
+                </Label>
+              </FormGroup>
+              <FormGroup check disabled>
+                <Label check>
+                  <Input type="radio" name="radio1" disabled />{' '}
+                  Option three is disabled
+                </Label>
+              </FormGroup>
+            </FormGroup>
+            <FormGroup check>
+              <Label check>
+                <Input type="checkbox" />{' '}
+                Check me out
+              </Label>
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+
+        </div>
+      </Row>
+    );
+  }
+}
+
+const mapStateToProps = storeState => ({
+  account: storeState.authentication.account,
+  isAuthenticated: storeState.authentication.isAuthenticated
+});
+
+const mapDispatchToProps = { getSession };
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CreateProject);
