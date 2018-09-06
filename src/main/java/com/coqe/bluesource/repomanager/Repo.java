f871138b116ee5ee25a0765e23c21ffd9569cbaa
Paul Reeves <paul.a.reeves@aexp.com>
2018-09-06 10:11:44 +0100

    Added Repo manager

---
+++ src/main/java/com/coqe/bluesource/repomanager/Repo.java
@@ -0,0 +1,7 @@
+package com.coqe.bluesource.repomanager;
+
+public interface Repo<T> {
+    
+    T get();
+    
+}
---
+++ src/main/java/com/coqe/bluesource/repomanager/RepoDetails.java
@@ -0,0 +1,32 @@
+package com.coqe.bluesource.repomanager;
+
+
+import java.util.Date;
+import java.util.Set;
+
+import com.google.common.base.Joiner;
+
+public interface RepoDetails<T extends Repo> {
+    
+    Joiner JOINER = Joiner.on(",").useForNull("");
+    
+    Set<String> getContributors(T repo);
+    
+    String getHomepage(T repo);
+    
+    String getFullname(T repo);
+    
+    String getDescription(T repo);
+    
+    String getLabels(T repo);
+    
+    int getOpenIssuesCount(T repo);
+    
+    String getOwnerName(T repo);
+    
+    Date getCreatedDate(T repo);
+    
+    default String getInfoCSV(T repo) {
+        return JOINER.join(getHomepage(repo), getFullname(repo), getDescription(repo), getCreatedDate(repo));
+    }
+}
